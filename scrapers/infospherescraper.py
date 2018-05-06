a9e8358dfcb426eda8663a54d8e117cfea5f4a07
David Bell <davidofbell@gmail.com>
2018-05-06 21:03:11 +0000

    now using SQLAlchemy and writing to the database. Still need to remove text inside the []

--- scrapers/infospherescraper.py
+++ scrapers/infospherescraper.py
@@ -1,62 +1,72 @@
-import _mysql
+from bs4 import BeautifulSoup
 import os
 import requests
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import string
-from collections import defaultdict
-from bs4 import BeautifulSoup
 
-page = requests.get("https://theinfosphere.org/Transcript:Space_Pilot_3000")
-character_lines_by_name = defaultdict(list)
+def main():
+    Base = declarative_base()
+    engine = create_engine('mysql://readwrite:'+os.environ["READ_WRITE_PASS"]+'@localhost/futurama')
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-if page.status_code !=requests.codes.ok:
-    print("Error fetching page")
-    exit
+    page = requests.get("https://theinfosphere.org/Transcript:Space_Pilot_3000")
 
-db= _mysql.connect("localhost","readwrite",os.environ["READ_WRITE_PASS"],"futurama")
+    if page.status_code ==requests.codes.ok:
+        parse_page_content(session, page.content)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-prose = soup.find_all('div', class_='poem')
-for x in prose:
-    try:
-        line =(x.get_text())
-        line = ''.join(filter(lambda x: x in string.printable, line))
-        split_on_bracket = line.partition(")")
-        time = (split_on_bracket[0]).strip()[1:]
-        minutes_and_seconds = time.partition(":")
-        seconds = (int(minutes_and_seconds[0]))*60 + (int(minutes_and_seconds[2]))
-        trimmed_out_time = split_on_bracket[2]
-        cleaned_line = trimmed_out_time.partition(":")
-        dialogue_line = Dialogue(1,1,cleaned_line[0].strip(),seconds, cleaned_line[2].strip())
-        
-    except Exception as e:
-        print(line)
-        print(e)
-
-def insert_into_db(dialogue):
-    """Function which takes in a line of Dialogue and writes it to the db.
+def parse_page_content(session, content):
+    """Parses the content of a requests request using the presets I've coded in for the Futuruma pages and inserts it into the db.
 
     Input:
-        dialogue (Dialogue): the line of dialogue. See below for class explanation.
-
+        session (sqlalchemy.Session): activated SQLAlachemy session we can use to insert the row.
+        content (content of a requests request): The content of the page we scraped.
+    Returns:
+        Nothing.
     """
     
+    soup = BeautifulSoup(content, 'html.parser')
+    prose = soup.find_all('div', class_='poem')
+    for x in prose:
+        try:
+            line =(x.get_text())
+            line = ''.join(filter(lambda x: x in string.printable, line))
+            split_on_bracket = line.partition(")")
+            time = (split_on_bracket[0]).strip()[1:]
+            minutes_and_seconds = time.partition(":")
+            seconds = (int(minutes_and_seconds[0]))*60 + (int(minutes_and_seconds[2]))
+            trimmed_out_time = split_on_bracket[2]
+            cleaned_line = trimmed_out_time.partition(":")
+            dialogue_line = Dialogue(season=1, episode=1, character_name=cleaned_line[0].strip(), episode_time=seconds, line=cleaned_line[2].strip())
+            session.add(dialogue_line)
+        except Exception as e:
+            print(line)
+            print(e)
 
-class Dialogue(object):
+    session.commit()
+   
+
+class Dialogue(declarative_base()):
     """Represents a line of dialogue from a tv show or movie.
 
     Attributes:
         season (int): Which season it's from.
         episode (int): episode number.
         character_name (str): Name of the character who says the line.
-        time (int): Time of the line in seconds from the start of the show.
+        episode_time (int): Time of the line in seconds from the start of the show.
         line (str): The actual line.
         """
 
-    def __init__(self, season, episode, character, time, line):
-        self.season = season
-        self.episode = episode
-        self.character_name = character
-        self.time = time
-        self.line = line
+    __tablename__ = "dialogue"
+    season = Column(Integer, primary_key = True)
+    episode = Column(Integer, primary_key = True)
+    character_name = Column(String, primary_key = True)
+    episode_time = Column(Integer, primary_key = True)
+    line = Column(String)
 
+
+if __name__ == "__main__":
+    main()
 
